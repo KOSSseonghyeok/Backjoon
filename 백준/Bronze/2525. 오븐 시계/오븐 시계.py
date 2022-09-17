eafa7e66ade90d1bd11ad1d327f974515c34455d
KOSSseonghyeok <104906731+KOSSseonghyeok@users.noreply.github.com>
2022-09-18 01:48:59 +0900

    [Bronze III] Title: 오븐 시계, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -2,15 +2,11 @@ ls = list(map(int, input().split()))
 hour = ls[0]
 min = ls[1]
 plusm = int(input())
-if min + plusm < 60:
-    fix_hour = hour
-    fix_min = min + plusm 
-else:
-    fix_hour = hour
-    fix_min = min + plusm 
-    while fix_min >= 60:
-        fix_hour += 1
-        fix_min -= 60
-        if fix_hour == 24:
-            fix_hour = 0
+fix_hour = hour
+fix_min = min + plusm 
+while fix_min >= 60:
+    fix_hour += 1
+    fix_min -= 60
+    if fix_hour == 24:
+        fix_hour = 0
 print(fix_hour, fix_min)
