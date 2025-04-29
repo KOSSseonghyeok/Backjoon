e840d757f5f530735f57a90ace5f8db69c3c1084
Hyeoki_iii <104906731+KOSSseonghyeok@users.noreply.github.com>
2025-04-29 15:58:35 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    
+    while(true){
+    int a = 0, b;
+    cin >> a >> b;
+    if(a == 0){
+        break;
+    }
+    cout << a+b << endl;
+    }
+    
+    return 0;
+}
